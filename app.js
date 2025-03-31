050d13b8fc8d70264434ddf0be42124693115de8
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2025-03-31 20:59:26 +0200

    exctract what is required

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 let pageNumber = 1;
 let pageSize = 10;
 
+const userContainer= document.querySelector(".user__container")
 const renderUser = (user) => {
+    let { name: { first, last },
+        location: { country },
+        email,
+        picture: { medium: userImage },
+    } = user;
     let htmStr = `
   <div class="user">
                     <div class="logo">
-                        <img class="user-logo" src="img/foto1.jpg" alt="user">
+                        <img class="user-logo" src="${userImage}" alt="user">
                     </div>
                     <div class="user-gebruiker">
-                    <div class="user-name">John Smith</div>
-                    <div class="user-country">Engeland</div>
-                    <div class="user-email">john.smith@gmail.com</div>
+                    <div class="user-name">${first} ${last}</div>
+                    <div class="user-country">${country}</div>
+                    <div class="user-email">${email}</div>
                 </div>
                 </div>`
-}
+};
 
 function getRandomUsers(pageNumber, pageSize) {
     let url = `https://randomuser.me/api/?page=1&results=10&seed=abc`;
@@ -22,6 +28,6 @@ function getRandomUsers(pageNumber, pageSize) {
         .then(data => {
             console.log(data.results);
         });
-}
+};
 
 getRandomUsers(pageNumber, pageSize);

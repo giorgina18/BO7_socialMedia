7919ddd4f53dd84b77bc4f6731c82a1c380f19d2
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2025-03-31 22:45:41 +0200

    infinite scroll

--- app.js
+++ app.js
@@ -1,33 +1,82 @@
 let pageNumber = 1;
 let pageSize = 10;
+let isPageLoad = true;
+const userContainer = document.querySelector(".users__container");
+const loadingEle = document.querySelector("#loading");
 
-const userContainer= document.querySelector(".user__container")
+const toggleLoading = (isLoading) => {
+  loadingEle.classList.toggle("show", isLoading);
+};
+
 const renderUser = (user) => {
-    let { name: { first, last },
-        location: { country },
-        email,
-        picture: { medium: userImage },
-    } = user;
-    let htmStr = `
-  <div class="user">
-                    <div class="logo">
-                        <img class="user-logo" src="${userImage}" alt="user">
-                    </div>
-                    <div class="user-gebruiker">
-                    <div class="user-name">${first} ${last}</div>
-                    <div class="user-country">${country}</div>
-                    <div class="user-email">${email}</div>
-                </div>
-                </div>`
+  let {
+    name: { first, last },
+    location: { country },
+    email,
+    picture: { medium: userImage },
+  } = user;
+  let htmlStr = `  <div class="user">
+          <div class="user-logo item">
+            <img src="${userImage}" alt="user" />
+          </div>
+          <div class="user-name item">${first} ${last}</div>
+          <div class="user-country item">${country}</div>
+          <div class="user-email item">${email}</div>
+        </div>`;
+  userContainer.insertAdjacentHTML("beforeend", htmlStr);
 };
 
-function getRandomUsers(pageNumber, pageSize) {
-    let url = `https://randomuser.me/api/?page=1&results=10&seed=abc`;
-    fetch(url)
-        .then(resp => resp.json())
-        .then(data => {
-            console.log(data.results);
-        });
+async function getRandomUsers(pageNumber, pageSize) {
+  let url = `https://randomuser.me/api/?page=${pageNumber}&results=${pageSize}&seed=abc`;
+  const resp = await fetch(url);
+  const data = await resp.json();
+  return data;
+}
+
+const getLastUseEle = () =>
+  document.querySelector(".users__container > .user:last-child");
+
+const loadUsers = (pageNumber, pageSize) => {
+  return new Promise((resolve, reject) => {
+    getRandomUsers(pageNumber, pageSize)
+      .then((data) => {
+        data &&
+          data.results &&
+          data.results.forEach((user) => renderUser(user));
+        if (isPageLoad) {
+          obseveLastUser();
+          isPageLoad = false;
+        }
+        resolve("Completed Rendering");
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+toggleLoading(true);
+loadUsers(pageNumber, pageSize)
+  .then((data) => {
+    toggleLoading(false);
+  })
+  .catch((error) => toggleLoading(false));
+
+const infScrollCallback = (entries, observer) => {
+  const entry = entries[0];
+  if (!entry.isIntersecting) return;
+  pageNumber += 1;
+  toggleLoading(true);
+  loadUsers(pageNumber, pageSize)
+    .then((resp) => {
+      obseveLastUser();
+      toggleLoading(false);
+    })
+    .catch((error) => toggleLoading(false));
+  observer.unobserve(entry.target);
 };
 
-getRandomUsers(pageNumber, pageSize);
+const infScrollObserver = new IntersectionObserver(infScrollCallback, {});
+
+const obseveLastUser = () => {
+  infScrollObserver.observe(getLastUseEle());
+};
